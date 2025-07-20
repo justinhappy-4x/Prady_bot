b15e0023ed288f470e52c2955a6f451ac187409d
montser_hunk <152070262+justinhappy-4x@users.noreply.github.com>
2025-07-20 06:53:50 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,12 @@
+# server.py
 from flask import Flask
 from threading import Thread
 
 app = Flask('')
 
-
 @app.route('/')
 def home():
     return "✅ Bot is running!"
 
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 8080}).start()
